84e9dc6cfba15bc80285e5dd583b1d78fd82f65d
Armando Terrazas Gómez <mandotrs@icloud.com>
2023-03-13 19:46:03 -0600

    El jugador puede disparar

--- Assets/Scripts/PlayerLaser.cs
+++ Assets/Scripts/PlayerLaser.cs
@@ -8,12 +8,13 @@ public class PlayerLaser : MonoBehaviour
     [SerializeField]private float speed = 2f;
     void Start()
     {
+        transform.Rotate(90, 0, 0); // Rotate the bullet
         Destroy(gameObject, 5f); // Destroy the bullet after 5 seconds
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(0, 1, 0) * Time.deltaTime * speed, Space.self); // Move the bullet    
+        transform.Translate(new Vector3(0, 1, 0) * Time.deltaTime * speed, Space.Self); // Move the bullet    
     }
 }
