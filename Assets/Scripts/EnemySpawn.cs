ce2c47754aa0e738a03ecdfc20752a1912d84a96
HeberM69 <hgmb15hjrmcmr@gmail.com>
2023-03-14 23:00:49 -0600

    Spawn enemigo terminado

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -6,26 +6,26 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
-    public GameObject asteroidPrefab;
+    public GameObject EnemyPrefab;
     public float spawnRadius = 20f;
     public float spawnRate = 2f;
-    public float asteroidSpeed = 10f;
+    public float EnemySpeed = 10f;
 
     [SerializeField]
     public float destructionTime;
+
+    public float spawnDistance = 10f;
 
     private Transform target;
     private float nextSpawnTime;
 
     void Start()
     {
-        // buscar la nave por etiqueta
         target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     void Update()
     {
-        // spawnear un asteroide si es tiempo de hacerlo
         if (Time.time >= nextSpawnTime)
         {
             SpawnAsteroid();
@@ -35,18 +35,15 @@ public class EnemySpawn : MonoBehaviour
 
     void SpawnAsteroid()
     {
-        // generar un punto aleatorio dentro del radio de spawn
         Vector3 spawnPosition = transform.position + Random.insideUnitSphere * spawnRadius;
-        spawnPosition.z = 0f; // asegurarse de que el asteroide no se genere en el eje z
+        spawnPosition.z = Random.Range(-spawnDistance, spawnDistance); 
+        
+        GameObject Enemy = Instantiate(EnemyPrefab, spawnPosition, Quaternion.identity);
+        Enemy.transform.LookAt(target);
 
-        // crear el asteroide en la posición generada y rotado hacia la nave
-        GameObject asteroid = Instantiate(asteroidPrefab, spawnPosition, Quaternion.identity);
-        asteroid.transform.LookAt(target);
+        Rigidbody EnemyRb = Enemy.GetComponent<Rigidbody>();
+        EnemyRb.velocity = Enemy.transform.forward * EnemySpeed;
 
-        // añadirle movimiento al asteroide
-        Rigidbody asteroidRb = asteroid.GetComponent<Rigidbody>();
-        asteroidRb.velocity = asteroid.transform.forward * asteroidSpeed;
-
-        Destroy(asteroid, destructionTime);
+        Destroy(Enemy, destructionTime);
     }
 }
--- Assets/Scripts/ParticlesMovement.cs
+++ Assets/Scripts/ParticlesMovement.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class ParticlesMovement : MonoBehaviour
 {
-    public Transform target; // Referencia al jugador
-    public float followSpeed = 10f; // Velocidad de seguimiento de las particulas
+    public Transform target;
+    public float followSpeed = 10f;
 
-    private ParticleSystem ps; // Referencia al sistema de particulas
-    private ParticleSystem.Particle[] particles; // Arreglo de particulas en el sistema
-    private int numParticlesAlive; // Numero de particulas en el sistema
+    private ParticleSystem ps;
+    private ParticleSystem.Particle[] particles;
+    private int numParticlesAlive;
 
     void Start()
     {
