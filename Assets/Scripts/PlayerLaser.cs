8f452f62ed280494d2ba35e7229000ef6b071073
HugoGoHe <A01640856@itesm.mx>
2023-03-18 10:44:21 -0600

    The Player's lasers destroy the cubes when collision occurs

--- Assets/Scripts/PlayerLaser.cs
+++ Assets/Scripts/PlayerLaser.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerLaser : MonoBehaviour
 {
+    [SerializeField]private float damage = 10f;
+
+    private Player player;
+
+
     // Start is called before the first frame update
     [SerializeField]private float speed = 2f;
     void Start()
@@ -16,5 +21,16 @@ public class PlayerLaser : MonoBehaviour
     void Update()
     {
         transform.Translate(new Vector3(0, 1, 0) * Time.deltaTime * speed, Space.Self); // Move the bullet    
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Enemy")){
+            other.GetComponent<Enemy>().Damage(damage);
+            Destroy(gameObject);
+            player.Score += 10;
+            print("dead");
+            //_scoreGUI._texto.text = "Score: " + player.Score;
+        }
     }
 }
